d23f6019bf279926a71d28fd45e869d4be96a975
Mariel Latella Barros <mariel.latella@gmail.com>
2018-03-12 16:56:00 +0100

    renamed project

--- PE_01/codeIgniter/application/views/pages/home.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo " Here enter some 'HOME' data ";
--- PE_01/codeIgniter/application/views/templates/header.php
+++
@@ -1,9 +0,0 @@
-<!--The header contains the basic HTML code that you’ll want to display before loading the main view, together with a heading. -->
-<!--It will also output the $title variable, which we’ll define later in the controller.-->
-<html>
-<head>
-    <title>CodeIgniter Tutorial</title>
-</head>
-<body>
-
-<h1><?php echo $title; ?></h1>
