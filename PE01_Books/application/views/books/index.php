5a35be65cc830e089b8b04107bfe95c4d93ecf01
Mariel Latella Barros <mariel.latella@gmail.com>
2018-03-20 10:34:59 +0100

    add buttons to navigate to the create view put the contents into a table

--- PE01_Books/application/views/books/index.php
+++ PE01_Books/application/views/books/index.php
@@ -1,16 +1,35 @@
-
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
 
 <h2><?php echo $titleHeader; ?></h2>
 
+<div class="navigation">
+    <form action="<?php echo base_url();?>books">
+        <button name="homeBtn" type="submit">Home</button>
+    </form>
+    <form action="<?php echo base_url();?>books/create">
+        <button type="submit">Add new book</button>
+    </form>
+</div>
+
+<table class="center">
+    <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Pages</th>
+        <th>Release date</th>
+    </tr>
+
 <?php foreach ($books as $books_item): ?>
+    <div class="container">
+        <tr>
+            <td><?php echo $books_item['id']; ?></td>
+            <td><?php echo $books_item['Title']; ?></td>
+            <td><?php echo $books_item['Author']; ?></td>
+            <td><?php echo $books_item['Pages']; ?></td>
+            <td><?php echo $books_item['ReleaseDate']; ?></td>
+        </tr>
 
-    <h3><?php echo $books_item['id']; ?></h3>
-    <div class="main">
-        <?php echo $books_item['Title']; ?>
-        <?php echo $books_item['Author']; ?>
-        <?php echo $books_item['Pages']; ?>
-        <?php echo $books_item['ReleaseDate']; ?>
     </div>
-
 <?php endforeach; ?>
+</table>
