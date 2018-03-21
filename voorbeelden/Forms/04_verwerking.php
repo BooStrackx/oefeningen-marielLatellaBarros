6bb417355122a69e22a139087b590bb5b1679ac5
Mariel Latella Barros <mariel.latella@gmail.com>
2018-03-21 08:48:02 +0100

    add a hidden button to send data already retrieved from a first page to another.

---
+++ voorbeelden/Forms/04_verwerking.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hidden input</title>
+</head>
+<body>
+De bevestigde waarde voor naam is <?php
+print($_GET['naam']);?>.
+</body>
+</html>
