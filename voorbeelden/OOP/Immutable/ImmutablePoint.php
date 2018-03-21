bfe47ba49da51cd2dd81ba731e80a1942a99f316
Mariel Latella Barros <mariel.latella@gmail.com>
2018-03-21 15:49:42 +0100

    create immutable objects. the change of an object triggers the creation of another

---
+++ voorbeelden/OOP/Immutable/ImmutablePoint.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class ImmutablePoint {
+
+    private $x; private $y;
+
+    public function __construct($x, $y) {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function changeX($x){
+        return new self($x,$this->y);
+    }
+
+    public function changeY($y){
+        return new self($this->x,$y);
+    }
+
+    public function getX() {
+        return $this->x;
+    }
+
+    public function getY() {
+        return $this->y;
+    }
+
+    function __toString() {
+        return "($this->x , $this->y) <br/>"; }
+}
