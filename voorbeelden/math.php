1f3bae175ce3ee387229b8a88f667ceb490c1955
Mariel Latella Barros <mariel.latella@gmail.com>
2018-03-21 08:37:11 +0100

    renamed / deleted

--- voorbeelden/math.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-    function som6 (int $getal1,int $getal2 ):int {
-        return $getal1 + $getal2;
-    }
-    ?>
--- voorbeelden/som.php
+++
@@ -1,6 +0,0 @@
-<?php
-//    --som.php
-require_once ( 'math.php' );
-$resultaat = som ( 1, 2 );
-print ( $resultaat ) ;
-?>
