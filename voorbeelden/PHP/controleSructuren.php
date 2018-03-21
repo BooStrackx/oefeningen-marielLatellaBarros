a959b0e6d4f2c8bdf8595930e050f85a248b5d8a
Mariel Latella Barros <mariel.latella@gmail.com>
2018-03-21 08:59:01 +0100

    init commit samples control structures and operations

---
+++ voorbeelden/PHP/controleSructuren.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Controle Structuren</title>
+</head>
+<body>
+<!--TODO: describe it-->
+</body>
+</html>
---
+++ voorbeelden/PHP/operaties.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Operaties</title>
+</head>
+<body>
+<!--TODO: describe it-->
+</body>
+</html>
